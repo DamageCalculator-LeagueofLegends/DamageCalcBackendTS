e8dbcd22e3d7a1817cc4bfe90734118baee28c32
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-04 20:50:43 +0100

    refactor: change forEach to for of

--- src/Item/Item.ts
+++ src/Item/Item.ts
@@ -1,36 +1,36 @@
-import { Url } from "../RawChampion/RawChampion"
-import { ItemStats } from "../RawItem/ItemStats"
-import { Passive } from "../RawItem/Passive"
-import { RawItem } from "../RawItem/RawItem"
+import { Url } from '../RawChampion/RawChampion';
+import { ItemStats } from '../RawItem/ItemStats';
+import { Passive } from '../RawItem/Passive';
+import { RawItem } from '../RawItem/RawItem';
 
 export abstract class Item {
-    itemID: number
-    itemName: string
-    itemIcon: Url
+  itemID: number;
+  itemName: string;
+  itemIcon: Url;
 
-    isLegendary: boolean
-    isMythic: boolean
+  isLegendary: boolean;
+  isMythic: boolean;
 
-    itemStats: ItemStats
-    mythicStats: ItemStats
+  itemStats: ItemStats;
+  mythicStats: ItemStats;
 
-    constructor(rawItem: RawItem){
-        this.itemID = rawItem.id
-        this.itemName = rawItem.name
-        this.itemIcon = rawItem.icon
+  constructor(rawItem: RawItem) {
+    this.itemID = rawItem.id;
+    this.itemName = rawItem.name;
+    this.itemIcon = rawItem.icon;
 
-        this.itemStats = rawItem.stats
-        
-        this.setMythicStats(rawItem.passives)
-    }
+    this.itemStats = rawItem.stats;
 
-    setMythicStats(itemPassives: Passive[]){
-        if (itemPassives?.length) {
-            itemPassives.forEach(element => {
-                if (element.mythic == true){
-                    this.mythicStats = element.stats
-                }
-            });
+    this.setMythicStats(rawItem.passives);
+  }
+
+  setMythicStats(itemPassives: Passive[]) {
+    if (itemPassives?.length) {
+      for (const passive of itemPassives ) {
+        if (passive.mythic == true) {
+          this.mythicStats = passive.stats;
         }
+      }
     }
+  }
 }
