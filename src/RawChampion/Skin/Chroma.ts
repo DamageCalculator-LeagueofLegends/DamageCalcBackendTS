b28a71b8982b9fe4bc532f91239bbaf4c74a438b
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-04 20:50:02 +0100

    refactor: format file

--- src/RawChampion/Skin/Chroma.ts
+++ src/RawChampion/Skin/Chroma.ts
@@ -1,20 +1,20 @@
 import { Url } from '../RawChampion';
 
 export interface Chroma {
-    name: string;
-    id: number;
-    chromaPath: Url;
-    colors: string[];
-    descriptions: ChromaDescription[];
-    rarities: ChromaRarity[];
+  name: string;
+  id: number;
+  chromaPath: Url;
+  colors: string[];
+  descriptions: ChromaDescription[];
+  rarities: ChromaRarity[];
 }
 
 export interface ChromaDescription {
-    description?: string | null;
-    region?: string | null;
+  description?: string | null;
+  region?: string | null;
 }
 
 export interface ChromaRarity {
-    rarity?: number | null;
-    region?: string | null;
+  rarity?: number | null;
+  region?: string | null;
 }
