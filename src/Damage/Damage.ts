bd7bcea1e34c32758dee931a7f72baa8f89b3c5f
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-05 16:47:24 +0100

    refactor: format file

--- src/Damage/Damage.ts
+++ src/Damage/Damage.ts
@@ -1,27 +1,24 @@
-import { DamageType } from "../RawChampion/abilities/staticDataEnums";
+import { DamageType } from '../RawChampion/abilities/staticDataEnums';
 
 export class Damage {
+  type: DamageType;
+  damage: number;
+  constructor(type: DamageType) {
+    this.type = type;
+    this.damage = 0;
+  }
 
-    type: DamageType
-    damage: number
-    constructor(type: DamageType) {
-        this.type = type
-        this.damage = 0
-    }
-
-    calculateDamge() {
-        let totalDamage: number = 0
-        const basicDamage: number = getBasicDamage()
-        const scaledDamage: number = getScaledDamage()
-
-    }
-
+  calculateDamge() {
+    let totalDamage: number = 0;
+    const basicDamage: number = getBasicDamage();
+    const scaledDamage: number = getScaledDamage();
+  }
 }
 
 function getBasicDamage(): number {
-    return 0
+  return 0;
 }
 
 function getScaledDamage(): number {
-    return 0
+  return 0;
 }
