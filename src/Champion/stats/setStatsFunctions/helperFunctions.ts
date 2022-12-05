09f655cf22e38afce1fa5eca56b49bedcbf35b61
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-05 22:28:50 +0100

    refactor: simplified functions and formatting

--- src/Champion/stats/setStatsFunctions/helperFunctions.ts
+++ src/Champion/stats/setStatsFunctions/helperFunctions.ts
@@ -6,56 +6,42 @@ export function multiplicativeCalc(
   itemStat: number
 ): number {
   if (itemStat !== 0) {
-    if (currentStat === 0) {
+    if (currentStat === 0) 
       return 1 - itemStat / 100;
-    } else {
+    else 
       return 1 - currentStat * (itemStat / 100);
-    }
+    
   }
   return 0;
 }
 
-export function getNumberOfLegendaryItems(items: Item[]) {
-  let count: number = 0;
-  if (items.length > 0) {
-    for (const item of items) {
-      if (item.isLegendary === true) count++;
-    }
-  }
-  return count;
+export function getNumberOfLegendaryItems(items: Item[]): number {
+  return items.filter(item => item.isLegendary).length;
 }
 
-export function checkForMythicItemAndGetMythicItem(items: Item[]) {
-  let mythic: Item | null = null;
-  if (items.length > 0) {
-    for (const item of items) {
-      if (item.isMythic === true) mythic = item;
-    }
-  }
-  return mythic;
+export function checkForMythicItemAndGetMythicItem(items: Item[]): Item | null {
+  return items.find(item => item.isMythic) || null;
 }
 
 export function setAttackSpeedBasedOnLevel(growth: number, level: number) {
-  let total = (growth / 100) * (level - 1) * (0.7025 + 0.175 * (level - 1));
+  const total = (growth / 100) * (level - 1) * (0.7025 + 0.175 * (level - 1));
   return total * 100;
 }
 
 export function setAttackSpeed(base: number, ratio: number, bonus: number) {
   let total = base + ratio * (bonus / 100);
   // TODO: there is an excpetion for some champs and other conditions needs to implemented at some point lol
-  if (total > 2.5) {
-    total = 2.5;
-  }
+  total = Math.min(total, 2.5)
   return total;
 }
 
 export function updateMultiplicativeStats(totalStats: TotalStats) {
   let currStats: TotalStats = { ...totalStats };
 
-  currStats.armorPenPercentage = 1 - currStats.armorPenPercentage;
-  currStats.magicPenPercentage = 1 - currStats.magicPenPercentage;
-  currStats.tenacity = 1 - currStats.tenacity;
-  currStats.slowResistance = 1 - currStats.slowResistance;
+  currStats.armorPenPercentage = 1 - totalStats.armorPenPercentage;
+  currStats.magicPenPercentage = 1 - totalStats.magicPenPercentage;
+  currStats.tenacity = 1 - totalStats.tenacity;
+  currStats.slowResistance = 1 - totalStats.slowResistance;
 
   return currStats;
 }
