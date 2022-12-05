c7cd5d98c5b048f4cfb6a7e65d1d791fc4ca8671
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-05 23:24:38 +0100

    refactor: change let to const

--- src/Champion/stats/setStatsFunctions/getStatBasedOnLevel.ts
+++ src/Champion/stats/setStatsFunctions/getStatBasedOnLevel.ts
@@ -1,8 +1,8 @@
 import { StatsInfo } from '../../../RawChampion/Stats';
 
-export function getStatBasedOnLevel(Stat: StatsInfo, level: number) {
-  let base: number = Stat.flat;
-  let growth: number = Stat.perLevel;
-  let total = base + growth * (level - 1) * (0.7025 + 0.175 * (level - 1));
+export function getStatBasedOnLevel(stat: StatsInfo, level: number) {
+  const base: number = stat.flat;
+  const growth: number = stat.perLevel;
+  const total = base + growth * (level - 1) * (0.7025 + 0.175 * (level - 1));
   return Math.round((total + Number.EPSILON) * 10000) / 10000;
 }
