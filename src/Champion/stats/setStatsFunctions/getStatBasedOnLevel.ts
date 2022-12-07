93f0cb35f703f8766f91c7442f823f92c4b7f8ef
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-07 23:31:04 +0100

    refactor: remove rounding of stats in getStatsBasedOnLevel

--- src/Champion/stats/setStatsFunctions/getStatBasedOnLevel.ts
+++ src/Champion/stats/setStatsFunctions/getStatBasedOnLevel.ts
@@ -4,5 +4,5 @@ export function getStatBasedOnLevel(stat: StatsInfo, level: number) {
   const base: number = stat.flat;
   const growth: number = stat.perLevel;
   const total = base + growth * (level - 1) * (0.7025 + 0.175 * (level - 1));
-  return Math.round((total + Number.EPSILON) * 10000) / 10000;
+  return total;
 }
