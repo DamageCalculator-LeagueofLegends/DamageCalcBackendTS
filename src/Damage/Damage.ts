8a2d3315177f71a6b358634217559276ee184ae0
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-07 21:41:41 +0100

    refactor: delete all methods from Damage class, only an object with two attributes and constructor

--- src/Damage/Damage.ts
+++ src/Damage/Damage.ts
@@ -1,24 +1,11 @@
 import { DamageType } from '../RawChampion/abilities/staticDataEnums';
 
 export class Damage {
-  type: DamageType;
-  damage: number;
-  constructor(type: DamageType) {
+  type: keyof typeof DamageType | null;
+  value: number = 0;
+  constructor(type: keyof typeof DamageType | null, value = 0) {
     this.type = type;
-    this.damage = 0;
-  }
-
-  calculateDamge() {
-    let totalDamage: number = 0;
-    const basicDamage: number = getBasicDamage();
-    const scaledDamage: number = getScaledDamage();
+    this.value = value;
   }
 }
 
-function getBasicDamage(): number {
-  return 0;
-}
-
-function getScaledDamage(): number {
-  return 0;
-}
