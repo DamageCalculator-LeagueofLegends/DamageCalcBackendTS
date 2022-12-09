c00ea547356fc0b4a522fae1bde44c6ceee9718d
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-10 00:22:12 +0100

    feat: add all the content for the action methods in the Aatrox class

--- src/championList/Aatrox.ts
+++ src/championList/Aatrox.ts
@@ -1,33 +1,93 @@
-import { Champion } from "../Champion/Champion";
-import { Damage } from "../Damage/Damage";
-import { RawChampion } from "../RawChampion/RawChampion";
+import {
+  AllConditions,
+  checkCondition,
+} from '../Ability/dynamicAbilityData/ActionConditions';
+import { Champion } from '../Champion/Champion';
+import { Damage } from '../Damage/Damage';
+import { DamageType } from '../RawChampion/abilities/staticDataEnums';
+import { RawChampion } from '../RawChampion/RawChampion';
 
 export class Aatrox extends Champion {
-    hasPassive = true
-    constructor(rawChampion: RawChampion){
-        super(rawChampion)
-    }
-    override autoAttack(): Damage | Damage[] | null {
-        return null
-    }
+  hasPassive = true;
+  constructor(rawChampion: RawChampion) {
+    super(rawChampion);
+  }
+  override autoAttack(): Damage | Damage[] | null {
+    const autoAttackDamage = new Damage(
+      DamageType.PHYSICAL_DAMAGE,
+      this.champTotalStats.attackDamage
+    );
+    if (!this.hasPassive) return autoAttackDamage;
 
-    override passiveAction(): Damage | Damage[] | null {
-        return null
-    }
+    const passiveDamage = this.passiveAction() as Damage;
+    this.hasPassive = false;
 
-    override qAction(): Damage | Damage[] | null {
-        return null
-    }
+    return [autoAttackDamage, passiveDamage];
+  }
 
-    override wAction(): Damage | Damage[] | null {
-        return null
-    }
+  override passiveAction(): Damage | Damage[] | null {
+    const damage = new Damage(DamageType.PHYSICAL_DAMAGE);
+    const amplifier = 0.0459 + 0.0041 * this.champLevel;
+    damage.value = this.champUtilInfo.enemyMaxHealth * amplifier;
+    return damage;
+  }
 
-    override eAction(): Damage | Damage[] | null {
-        return null
+  override qAction(): Damage | Damage[] | null {
+    const q = this.champAbilities.Q;
+    const { conditions: conditions } = q.dynamicData.actionConditions;
+    if (q.checkIfInsideBounds()) {
+      const attributeCondition = checkCondition(
+        conditions,
+        AllConditions.numberOfAttribute
+      );
+      const sweetSportCondition = checkCondition(
+        conditions,
+        AllConditions.isSweetSpot
+      );
+      const { hasCondition, index } = attributeCondition;
+      for (let i = 0; i < 3; i++) {
+        if (hasCondition === true)
+          if (conditions[index!]!.value === i)
+            if (
+              sweetSportCondition.hasCondition === true &&
+              conditions[sweetSportCondition.index!]?.value === true
+            )
+              return q.getDamage({ ability: 0, effect: i + 2, leveling: 1 });
+        return q.getDamage({ ability: 0, effect: i + 2, leveling: 0 });
+      }
     }
+    return null;
+  }
 
-    override rAction(): Damage | Damage[] | null {
-        return null
+  override wAction(): Damage | Damage[] | null {
+    const w = this.champAbilities.W;
+    const { conditions: conditions } = w.dynamicData.actionConditions;
+    if (w.checkIfInsideBounds()) {
+      const tetherCondition = checkCondition(
+        conditions,
+        AllConditions.tetherCompleted
+      );
+      const { hasCondition, index } = tetherCondition;
+      if (hasCondition === true && conditions[index!]?.value === true)
+        return w.getDamage({ ability: 0, effect: 2, leveling: 0 });
+      return w.getDamage();
     }
+    return null;
+  }
+
+  override eAction(): Damage | Damage[] | null {
+    return null;
+  }
+
+  override rAction(): Damage | Damage[] | null {
+    const r = this.champAbilities.R;
+    r.setAttributeIndicies({ ability: 0, effect: 2, leveling: 0 });
+    if (r.checkIfInsideBounds()) {
+      const adValue =
+        r.getAbilityModifiers()[0]?.values[r.dynamicData.skillLevel]!;
+      const bonusADFromR = this.champTotalStats.attackDamage * (adValue / 100);
+      this.updateTotalBonusAndScalingValues(bonusADFromR, 'attackDamage');
+    }
+    return null;
+  }
 }
