c2156a7858446e5e4d6c911003f799f4b584462f
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-12-10 15:53:41 +0100

    feat: implement passiveAction and autoAttack methods for Akali class

--- src/championList/Akali.ts
+++ src/championList/Akali.ts
@@ -1,18 +1,44 @@
 import { Champion } from '../Champion/Champion';
 import { Damage } from '../Damage/Damage';
+import { DamageType } from '../RawChampion/abilities/staticDataEnums';
 import { RawChampion } from '../RawChampion/RawChampion';
 
 export class Akali extends Champion {
+  hasAssissinsMark = false;
   constructor(rawChampion: RawChampion) {
     super(rawChampion);
   }
 
   override autoAttack(): Damage[] {
-    return [];
+    const autoAttack = new Damage(
+      DamageType.PHYSICAL_DAMAGE,
+      this.champTotalStats.attackDamage
+    );
+    const passiveDamage = this.passiveAction()[0];
+    if (this.hasAssissinsMark) {
+      this.hasAssissinsMark = false;
+      return [autoAttack, passiveDamage!];
+    }
+    return [autoAttack];
   }
 
   override passiveAction(): Damage[] {
-    return [];
+    const adAmp = this.champBonusStats.attackDamage * 0.6;
+    const apAmp = this.champTotalStats.abilityPower * 0.55;
+    let baseDamage = 32;
+
+    if (this.champLevel <= 7) baseDamage += 3 * this.champLevel;
+    else if (this.champLevel <= 13) {
+      baseDamage += 3 * 7;
+      baseDamage += 9 * (this.champLevel - 7);
+    } else {
+      baseDamage += 3 * 7;
+      baseDamage += 9 * 6;
+      baseDamage += 15 * (this.champLevel - 13);
+    }
+    const totalDamage = baseDamage + adAmp + apAmp;
+    const damage = new Damage(DamageType.MAGIC_DAMAGE, totalDamage);
+    return [damage];
   }
 
   override qAction(): Damage[] {
